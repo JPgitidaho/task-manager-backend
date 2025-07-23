daba5e4791fb4c49fc078d49a008d5660dfd2601
JPgitidaho <juanichile2015@gmail.com>
2025-07-23 19:44:54 -0400

    actualization

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://proyecto-fronted-fullstack.vercel.app'
+  ],
   credentials: true
 }))
+
 
 app.use(express.json())
 app.use('/api/tasks', taskRoutes)
