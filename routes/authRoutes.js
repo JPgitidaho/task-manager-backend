cfe84063ee8266c6121cfa8c9825fdb21c588598
JPgitidaho <juanichile2015@gmail.com>
2025-07-23 19:30:33 -0400

    actualization

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { register, login } from '../controllers/authController.js'
+import { registerValidator, loginValidator } from '../validators/authValidator.js'
+import handleValidation from '../middleware/handleValidation.js'
 
 const router = express.Router()
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', registerValidator, handleValidation, register)
+router.post('/login', loginValidator, handleValidation, login)
 
 export default router
